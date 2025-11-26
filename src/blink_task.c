61394585d0acaaeb3314bbef1ad80d02e17d316b
Cubby DeBry <cubbydebry@gmail.com>
2025-11-26 11:48:53 -0700

    Tweaked blink task, chnaged timing to match blink.

--- src/blink_task.c
+++ src/blink_task.c
@@ -2,13 +2,7 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-
-
-#ifndef PICO_DEFAULT_LED_PIN
-#define LED_PIN 25
-#else
-#define LED_PIN PICO_DEFAULT_LED_PIN
-#endif
+#define LED_PIN 21
 
 void vBlinkTask(void *blink){
     (void)blink;
@@ -16,16 +10,16 @@ void vBlinkTask(void *blink){
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    cyw43_arch_init();
+    // cyw43_arch_init();
     
     for (;;) {
         gpio_put(LED_PIN, 1);
 
-        vTaskDelay(pdMS_TO_TICKS(250));
+        vTaskDelay(pdMS_TO_TICKS(500));
 
         gpio_put(LED_PIN, 0);
 
-        vTaskDelay(pdMS_TO_TICKS(250));
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
 
